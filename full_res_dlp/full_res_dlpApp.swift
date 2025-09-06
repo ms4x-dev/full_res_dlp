b4b659a0c2b9e48c6a988547a1c020f67c45bde0
ms4x-dev <ms4x@proton.me>
2025-09-06 10:53:05 +1000

    Push1

---
+++ full_res_dlp/full_res_dlpApp.swift
@@ -0,0 +1,17 @@
+//
+//  full_res_dlpApp.swift
+//  full_res_dlp
+//
+//  Created by Richard on 6/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct full_res_dlpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ full_res_dlpTests/full_res_dlpTests.swift
@@ -0,0 +1,17 @@
+//
+//  full_res_dlpTests.swift
+//  full_res_dlpTests
+//
+//  Created by Richard on 6/9/2025.
+//
+
+import Testing
+@testable import full_res_dlp
+
+struct full_res_dlpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
